6b5f143e33b181ee32af1eab45eafaa4793941aa
hiddencilantro <stevekim1109@gmail.com>
2021-08-20 13:35:56 -0400

    commit tree 8272163301b6613fd5a75504f43dbbbf16a60d4b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(sourceArray, fn) {
+    const newArray = [];
+    for (const element of sourceArray) {
+        newArray.push(fn(element));
+    };
+    return newArray;
+}
+
+function reduce(sourceArray, fn, startingValue) {
+    let total;
+    if (startingValue) {
+        total = startingValue;
+        for (const element of sourceArray) {
+            total = fn(element, total);
+        };
+        return total;
+    } else {
+        total = sourceArray[0];
+        for (let i=1; i<sourceArray.length; i++) {
+            total = fn(sourceArray[i], total);
+        };
+        return total;
+    };
+}
